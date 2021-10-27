9bae8e5bd40a9c2f7ddf0f43b69866fbceb39726
Anita Safar <ancsa0416@gmail.com>
2021-10-27 13:45:20 +0100

    class and list done

---
+++ models/cakes.py
@@ -0,0 +1,6 @@
+from models.order import *
+
+order1 = Order("John Doe", "2021.10.27.", 6, "muffins")
+order2 = Order("Jane Austin", "1865.08.09.", 13, "doughnuts")
+order3 = Order("Donald Duck", "2005.05.25.", 1, "white bloomer")
+orders = [order1, order2, order3]
