2953331e837d56e5f7acf85901103347b142aeef
Anita Safar <ancsa0416@gmail.com>
2021-10-27 14:52:17 +0100

    new routes created

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,1 +1,11 @@
 from flask import render_template
+from app import app
+from models.cakes import orders
+
+@app.route('/orders')
+def index():
+    return render_template('index.html', title="Cake Shop", orders=orders)
+
+@app.route('/orders/<order_number>')
+def find_order(order_number):
+    return render_template('order.html', title="Cake Shop", item=orders[int(order_number)-1])
--- models/cakes.py
+++ models/cakes.py
@@ -1,6 +1,6 @@
 from models.order import *
 
-order1 = Order("John Doe", "2021.10.27.", 6, "muffins")
-order2 = Order("Jane Austin", "1865.08.09.", 13, "doughnuts")
-order3 = Order("Donald Duck", "2005.05.25.", 1, "white bloomer")
+order1 = Order("John Doe", "2021.10.27.", 6, "Muffins")
+order2 = Order("Jane Austin", "1865.08.09.", 13, "Doughnuts")
+order3 = Order("Donald Duck", "2005.05.25.", 1, "White bloomer")
 orders = [order1, order2, order3]
