65c18cfaf52ae4b002a71df158a69090596d3b4e
Anita Safar <ancsa0416@gmail.com>
2021-10-27 15:28:59 +0100

    styling done

--- models/cakes.py
+++ models/cakes.py
@@ -1,6 +1,7 @@
 from models.order import *
 
-order1 = Order("John Doe", "2021.10.27.", 6, "Muffins")
-order2 = Order("Jane Austin", "1865.08.09.", 13, "Doughnuts")
-order3 = Order("Donald Duck", "2005.05.25.", 1, "White bloomer")
+order1 = Order("John Doe", "2021.10.27.", 6, "Muffins", 1)
+order2 = Order("Jane Austin", "1865.08.09.", 13, "Doughnuts", 2)
+order3 = Order("Donald Duck", "2005.05.25.", 1, "White bloomer", 3)
 orders = [order1, order2, order3]
+
